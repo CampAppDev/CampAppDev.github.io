6cdf58685cb8db2f8be8ec7feaf35dfe3d42ae9c
camp app dev <campappdev@gmail.com>
2016-09-17 15:09:27 -0400

    add about us

---
+++ aboutus.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+
+
+    $('.team-member a').click(function () {
+
+        var selected = $(this).attr('value');
+        var id = "#" + selected;
+        
+        $('.team-member a').each(function () {
+
+            var link = $(this);
+            if (link.attr('value') == selected) {
+                if (!link.hasClass('selected')) {
+                    link.addClass('selected');
+                }
+            }
+            else {
+                if (link.hasClass('selected')) {
+
+                    link.removeClass("selected");
+                }
+            }
+        });
+
+        $('.about').each(function () {
+
+            var about = $(this);
+            if (this.id == selected) {
+                if (!about.hasClass('current')) {
+                    about.addClass('current');
+
+                }
+            }
+
+            else {
+
+                if (about.hasClass('current')) {
+                    about.removeClass('current');
+
+                }
+            }
+
+
+        });
+
+    });
+
+
+
+});
