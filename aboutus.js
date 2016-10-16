565a38942e078e6abf6f9799d6314cc75d8ba2c6
camp app dev <campappdev@gmail.com>
2016-10-15 22:25:55 -0400

    ready

--- aboutus.js
+++ aboutus.js
@@ -1,10 +1,45 @@
 ﻿$(document).ready(function () {
 
+    var url = document.URL;
+    var id = url.substring(url.lastIndexOf('?') + 1);
+    var found = false;
+
+
+    $('.team-member a').each(function () {
+
+            var link = $(this);
+            if (link.attr('value') == id) {
+                if (!link.hasClass('selected')) {
+                    link.addClass('selected');
+                }
+                found = true;
+            }
+            else {
+                if (link.hasClass('selected')) {
+
+                    link.removeClass("selected");
+                }
+            }
+    });
+
+    $('.about').each(function(){
+        var link = $(this);
+            if (this.id == id) {
+                if (!link.hasClass('current')) {
+                    link.addClass('current');
+                }
+                found = true;
+            }
+            else {
+                if (link.hasClass('current')) {
+                    link.removeClass("current");
+                }
+            }
+    });
 
     $('.team-member a').click(function () {
 
         var selected = $(this).attr('value');
-        var id = "#" + selected;
         
         $('.team-member a').each(function () {
 
@@ -43,6 +78,12 @@
         });
 
     });
+
+    if (!found){
+        console.log("Here");
+            $('#default').trigger('click');
+    }
+
 
 
 
