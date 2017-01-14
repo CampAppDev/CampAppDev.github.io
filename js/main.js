ac6c24913362e97ef7f045dab8df325f86534df7
camp app dev <campappdev@gmail.com>
2017-01-14 02:14:08 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
   
+  $('#signUp').click(function(){
+    var email = $('#emailSignUp').val()
+    if (validateEmail(email)){
+      alert("Please enter a valid email");
+
+
+    }
+    else{
+      return;
+      // parameters: service_id, template_id, template_parameters
+    emailjs.send("default_service","signup",{"email": email}).then(
+        function(response) 
+        {
+          alert("Sign up successful!");
+        }, 
+        function(err) 
+        {
+    
+          alert("There was an error signing you up! Please try again");
+        }
+      );
+    }
+
+    return false;
+
+  });
   
   //------------------------------------//
   //Navbar//
@@ -43,3 +69,8 @@ $(document).ready(function(){
 
 	
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
